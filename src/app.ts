9734ce9833a17e3b7d7e87e8aa8b5f268e778b11
mihago <gomihagle@gmail.com>
2023-02-22 20:53:45 +0200

    init

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,21 @@
+import config from 'config'
+import cookieParser from 'cookie-parser'
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import path from 'path'
+import http from 'http'
 import https from 'https'
+import fs from 'fs'
+
+
+
+
+
+const HTTP_PORT = config.get('http_port')
+const HTTPS_PORT = config.get('https_port')
+
+const app = express()
+
+app.use(express.json())
+app.use(cookieParser())
