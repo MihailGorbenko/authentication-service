f4e046e7e6744694c02a4feadf051abc292a31bd
mihago <gomihagle@gmail.com>
2023-02-22 21:31:21 +0200

    server base logic done

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,16 @@ import config from 'config'
 import cookieParser from 'cookie-parser'
 import express from 'express'
 import cors from 'cors'
-import mongoose from 'mongoose'
+import mongoose, { Error } from 'mongoose'
 import path from 'path'
 import http from 'http'
 import https from 'https'
 import fs from 'fs'
+import Log from './utils/log'
 
 
 
-
+const log = new Log("app")
 
 const HTTP_PORT = config.get('http_port')
 const HTTPS_PORT = config.get('https_port')
@@ -19,3 +20,27 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+
+async function start() {
+    try {
+        mongoose.set('strictQuery',false)
+        mongoose.connect(config.get('mongoUri'))
+            .then(() => log.info('Mongo DB connection successfull'))
+        mongoose.connection.on('error', (err) => log.error(err))
+
+        http.createServer(app).listen(HTTP_PORT, () => log.info(`HTTP server listening on port ${HTTP_PORT}`))
+
+        https.createServer({
+            key: fs.readFileSync(path.resolve(__dirname, 'sslcert/privkey.pem')),
+            cert: fs.readFileSync(path.resolve(__dirname, 'sslcert/cert.pem')),
+            ca: fs.readFileSync(path.resolve(__dirname, 'sslcert/chain.pem'))
+        }, app).listen(HTTPS_PORT, () => log.info(`HTTPS server listening on port ${HTTPS_PORT}`))
+
+    } catch (e: Error | any) {
+        log.error(`Server error ${e}`)
+        process.exit(1)
+    }
+}
+
+
+start()
