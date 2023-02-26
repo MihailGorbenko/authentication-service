5f723aef00106b1208c6688c8ef3ebefb1b232de
mihago <gomihagle@gmail.com>
2023-02-26 21:22:38 +0200

    implemented register endpoint

---
+++ src/router/endpoints/register.ts
@@ -0,0 +1,77 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import { body, validationResult } from "express-validator";
+import config from "config";
+import Log from "../../utils/log";
+import ServiceUser from "../../models/ServiceUser";
+import { ResponceStatus } from "../responce_status";
+import { Router } from "express";
+import allowedEmail from "../../middleware/userRegistred";
+
+const registerRouter = Router();
+const log = new Log("Route: /register");
+
+registerRouter.post(
+  "/",
+  [
+    allowedEmail,
+    body("password", "bad password").isString().isLength({ min: 5 }),
+  ],
+  async (req: Request, res: Response) => {
+    
+    try {
+      ///// Validating request params
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(ResponceStatus.BadRequest).json({
+          message: "Incorect credentials",
+          predicate: "INCORECT",
+          errors: errors.array(),
+        });
+      }
+      //////////////////////////////////////////
+
+      const { email, password } = req.body;
+
+      ///// Check email 
+      if (req.user) {
+        log.info(`User ${email} already exists`);
+        return res.status(ResponceStatus.BadRequest).json({
+          message: "User already registred",
+          predicate: "EXIST",
+        });
+      }
+
+      //// Create new service user
+      log.info(`Creating service user ${email}`);
+
+      const hashPswd = bcrypt.hashSync(password, config.get("passwordSalt"));
+
+      const serviceUser = new ServiceUser({ email, password: hashPswd })
+        .save()
+        .then(
+          (onfullfiled) => {
+            log.info("User saved successfully");
+            return res.status(ResponceStatus.Success).json({
+              message: "User registred successfully",
+            });
+          },
+          (onrejected) => {
+            log.error("Error saving user");
+            return res.status(ResponceStatus.StorageError).json({
+              message: "Error saving user",
+            });
+          }
+        );
+
+      ////////////////////////////////////////////
+    } catch (e: Error | any) {
+      log.error(`Error ${e?.message}`);
+      return res.status(ResponceStatus.ServerError).json({
+        message: `Server error ${e?.message}`,
+      });
+    }
+  }
+);
+
+export default registerRouter;
